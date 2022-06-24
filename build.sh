4714ef272bf0986290d7b1944a417214c2680b80
Trần Trung Tín <trungtindev@gmail.com>
2022-06-24 12:00:04 +0700

    use tag v3.0.1

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ done
 cp $CDIR/zshrc $build_dir/.zshrc
 
 # tag=$(curl --silent https://api.github.com/repos/romkatv/zsh-bin/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
-tag=v6.1.1
+tag=v3.0.1
 distfile=zsh-5.8-linux-aarch64
 url="https://github.com/romkatv/zsh-bin/releases/download/$tag/$distfile.tar.gz"
 
